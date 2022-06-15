815482fcbe5629096736d6a7e965bb7eee3647c0
Gabriel Frasson <bielfc27@gmail.com>
2022-06-15 01:29:28 -0300

    Update router usage

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
 
+import { brandsRoutes } from "./brands.routes";
+import { productsRoutes } from "./products.routes";
+
 const router = Router();
+
+router.use("/brands", brandsRoutes);
+router.use("/products", productsRoutes);
+// router.use("/users", usersRoutes);
 
 export { router };
