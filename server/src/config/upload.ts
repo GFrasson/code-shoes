91478922562c16ee1c0c525be4dc4fc14004e9b0
Gabriel Frasson <bielfc27@gmail.com>
2022-06-15 11:20:17 -0300

    Fix image upload

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -2,16 +2,18 @@ import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
-export default function upload() {
-    return {
-        storage: multer.diskStorage({
-            destination: resolve(__dirname, "..", "..", "public", "img"),
-            filename: (request, file, callback) => {
-                const fileHash = crypto.randomBytes(16).toString("hex");
-                const fileName = `${fileHash}-${file.originalname}`;
+export default {
+    upload(folder: string) {
+        return {
+            storage: multer.diskStorage({
+                destination: resolve(__dirname, "..", "..", folder),
+                filename: (request, file, callback) => {
+                    const fileHash = crypto.randomBytes(16).toString("hex");
+                    const fileName = `${fileHash}-${file.originalname}`;
 
-                return callback(null, fileName);
-            },
-        }),
-    };
-}
+                    return callback(null, fileName);
+                },
+            }),
+        };
+    },
+};
