684ae99cc8722cd67d259aeed8357d1abe761a82
Gabriel Frasson <bielfc27@gmail.com>
2022-06-15 02:01:11 -0300

    New endpoints for list and show use cases

--- server/src/routes/brands.routes.ts
+++ server/src/routes/brands.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 
 import createBrandController from "../modules/brands/useCases/createBrand";
+import listBrandsController from "../modules/brands/useCases/listBrands";
 
 const brandsRoutes = Router();
+
+brandsRoutes.get("/", async (req, res) => {
+    await listBrandsController().handle(req, res);
+});
 
 brandsRoutes.post("/", async (req, res) => {
     await createBrandController().handle(req, res);
--- server/src/routes/products.routes.ts
+++ server/src/routes/products.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
 
 import createProductController from "../modules/products/useCases/createProduct";
+import listProductsController from "../modules/products/useCases/listProducts";
+import showProductController from "../modules/products/useCases/showProduct";
 
 const productsRoutes = Router();
+
+productsRoutes.get("/", async (req, res) => {
+    await listProductsController().handle(req, res);
+});
+
+productsRoutes.get("/:id", async (req, res) => {
+    await showProductController().handle(req, res);
+});
 
 productsRoutes.post("/", async (req, res) => {
     await createProductController().handle(req, res);
