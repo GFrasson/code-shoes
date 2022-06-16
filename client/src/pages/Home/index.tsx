d5b08809e28b2353a538f6068f6ebe1c487d97aa
Gabriel Frasson <bielfc27@gmail.com>
2022-06-16 18:55:48 -0300

    Home page without carousel

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { PageContainer } from '../../GlobalStyle';
+import { FlatCard } from '../../components/FlatCard';
+import { Navbar } from '../../components/Navbar';
 import { HomeContainer } from './style';
 
 export function Home() {
-  return (
-    <PageContainer>
-      <HomeContainer>
-        
-      </HomeContainer>
-    </PageContainer>
-  )
+    return (
+        <HomeContainer>
+            <Navbar />
+
+            <main>
+                <div className="page-content">
+                    <div className="content-container">
+                        <h1 className='product-name-title'>Tênis Nike - Running</h1>
+                        <p className='price text-primary-300'>R$199,90</p>
+                    </div>
+                    <div className="carousel-container">
+                        
+                    </div>
+                </div>
+                <div className="flat-cards-container">
+                    <FlatCard />
+                    <FlatCard />
+                    <FlatCard />
+                </div>
+            </main>
+        </HomeContainer>
+    )
 }
--- client/src/pages/Home/style.ts
+++ client/src/pages/Home/style.ts
@@ -1,5 +1,55 @@
 import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
+    height: 100vh;
+    color: var(--light-300);
     
+    main {
+        position: relative;
+
+        .page-content {
+            display: flex;
+            justify-content: center;
+            min-height: calc(100vh - 6.6rem);
+            height: auto;
+
+            .content-container {
+                width: 50%;
+                height: 100%;
+                padding-top: 2rem;
+                padding-left: 6.25rem;
+                margin-bottom: 12rem;
+
+                .product-name-title {
+                    word-break: break-all;
+                    text-transform: uppercase;
+                    max-width: 25rem;
+                    line-height: 7rem;
+                    font-size: 6.125rem;
+                    font-family: Staatliches;
+                }
+            
+                .price {
+                    font-weight: 400;
+                    font-size: 1.75rem;
+                }
+            }
+
+            .carousel-container {
+                width: 50%;
+                height: auto;
+                background-color: var(--dark-500);
+            }
+        }
+
+        .flat-cards-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 1rem 6.25rem;
+
+            margin-top: -12rem;
+            width: 100%;
+        }
+    }
 `;
