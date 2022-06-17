3edd155a0fbc8362898cbe50cada26d25b68cc27
Gabriel Frasson <bielfc27@gmail.com>
2022-06-16 22:40:32 -0300

    Starting products page

--- client/src/pages/Products/index.tsx
+++ client/src/pages/Products/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Card } from '../../components/Card';
 import { Navbar } from '../../components/Navbar';
-import { ProductsContainer } from './style';
 
+import { ProductsContainer } from './style';
 
 export function Products() {
     return (
@@ -9,15 +10,50 @@ export function Products() {
             <Navbar />
 
             <main className='products-page'>
-                <input
-                    className='search'
-                    type='text'
-                    placeholder='Nome do produto...'
-                />
-                <div id='productsContainer'>
-                    {/* INSERIR OS PRODUTOS AQUI */}
-                </div>
+                <div className="filter">
+                    <div className="form-group">
+                        <label
+                            htmlFor="product"
+                            className="block text-sm font-medium text-light-300"
+                        >
+                            Nome
+                        </label>
+                        <input
+                            className="mr-4 w-[39rem] text-gray-700 focus:ring-primary-300 focus:border-primary-300 shadow-sm sm:text-sm rounded-[0.625rem]"
+                            type='text'
+                            id='product'
+                            name='product'
+                            autoComplete="given-product"
+                            placeholder='Nome do produto...'
+                        />
+                    </div>
 
+                    <div className="form-group">
+                        <label
+                            htmlFor="brand"
+                            className="block text-sm font-medium text-light-300"
+                        >
+                            Marca
+                        </label>
+                        <select
+                            id="brand"
+                            name="brand"
+                            autoComplete="brand-name"
+                            className="py-2 px-3 border w-[10.6rem] text-gray-700 bg-light-300 rounded-[0.625rem] shadow-sm focus:outline-none focus:ring-primary-300 focus:border-primary-300 sm:text-sm"
+                        >
+                            <option>Nike</option>
+                            <option>Adidas</option>
+                            <option>Polo</option>
+                        </select>
+                    </div>
+                </div>
+                <div id='productsContainer'>
+                    <Card 
+                        name='Tênis Nike - Blue'
+                        brand='Nike'
+                        price={199.90}
+                    />
+                </div>
             </main>
         </ProductsContainer>
     );
--- client/src/pages/Products/style.ts
+++ client/src/pages/Products/style.ts
@@ -4,9 +4,17 @@ export const ProductsContainer = styled.div`
     .products-page {
         margin: 4rem 6.25rem;
 
-        .search {
-            padding: 0.5rem;
-            border-radius: 1.25rem;
+        .filter {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .search {
+                margin-right: 1rem;
+                padding: 0.8rem;
+                border-radius: 1.25rem;
+            }
         }
+
     }
 `;
