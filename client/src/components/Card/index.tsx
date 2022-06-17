5d1be657bddc3be3d25d3bb0c6386d8ca6fd31c8
Gabriel Frasson <bielfc27@gmail.com>
2022-06-16 22:40:19 -0300

    Card component

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,41 +1,30 @@
 import React from 'react';
-import './style.css';
+import { CardContainer } from './style';
+import { ShoppingCart } from 'phosphor-react';
 
 interface cardProps {
-  name: string;
-  brand: string;
-  price: number;
+    name: string;
+    brand: string;
+    price: number;
 }
 
 export function Card({ name, brand, price }: cardProps) {
-  return (
-    <div className="el-wrapper">
-      <div className="box-up">
-        <img
-          className="img"
-          src="https://m.media-amazon.com/images/I/81W9dIVSmuL._AC_SX695_.jpg"
-          alt=""
-        />
-        <div className="img-info">
-          <div className="info-inner">
-            <span className="p-name">{name}</span>
-            <span className="p-company">{brand}</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="box-down">
-        <div className="h-bg">
-          <div className="h-bg-inner"></div>
-        </div>
-
-        <a className="cart" href="#">
-          <span className="price">R${price}</span>
-          <span className="add-to-cart">
-            <span className="txt">Adicionar ao carrinho</span>
-          </span>
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <CardContainer>
+            <div className="images-container">
+                <img className='logo-image' src="../../../img/brands/nike.png" alt="Imagem da marca do tênis" />
+                <img className="shoe-image" src="../../../img/products/nike-blue.png" alt="Imagem de um tênis" />
+            </div>
+            <div className="content-box">
+                <div className="content">
+                    <div className="title">{ name }</div>
+                    <small className="price">R${ price.toFixed(2) }</small>
+                </div>
+            </div>
+            <button className='cart-button'>
+                <ShoppingCart size={16} />
+                <span>+ Carrinho</span>
+            </button>
+        </CardContainer>
+    );
 }
---
+++ client/src/components/Card/style.ts
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+    width: 16.25rem;
+    height: 18.125rem;
+    background-color: var(--light-300);
+    border-radius: 1.25rem;
+    position: relative;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+        outline: 2px solid var(--primary-300);
+        outline-offset: 2px;
+    }
+
+    .images-container {
+        padding: 0.5rem;
+
+        .logo-image {
+            position: absolute;
+            top: 0.5rem;
+            left: 0.8rem;
+            width: 1.5rem;
+            height: 1.5rem;
+            object-fit: contain;
+        }
+
+        .shoe-image {
+            width: 100%;
+            height: 7.8rem;
+            object-fit: contain;
+        }
+    }
+
+    .content-box {
+        font-weight: 600;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+        
+        .content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+            .title {
+                color: var(--dark-300);
+                font-size: 1.5rem;
+            }
+
+            .price {
+                font-size: 0.875rem;
+                color: var(--primary-300);
+            }
+        }
+    }
+
+    .cart-button {
+        background-color: var(--primary-300);
+        width: 8.75rem;
+        height: 1.6rem;
+        padding: 0.25rem 1rem;
+        border-radius: 0.625rem;
+        color: var(--light-300);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: all 0.2s;
+
+        span {
+            margin-left: 0.1rem;
+            font-weight: 600;
+            font-size: 0.875rem;
+        }
+
+        &:hover {
+            background-color: var(--primary-500);
+        }
+    }
+`; 
