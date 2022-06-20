be65e6cc7df9d10b3180d158b5b88590d77b6cf6
Gabriel Frasson <bielfc27@gmail.com>
2022-06-20 17:56:59 -0300

    New product page

--- client/src/pages/NewProduct/index.tsx
+++ client/src/pages/NewProduct/index.tsx
@@ -1,10 +1,95 @@
-import React from 'react';
-import { PageContainer } from '../../GlobalStyle';
+import React, { useState } from 'react';
+import { Navbar } from '../../components/Navbar';
+import { ImageInput } from '../../components/ImageInput';
+
+import { NewProductContainer } from './style';
 
 export function NewProduct() {
-  return (
-    <PageContainer>
-      <h1>Cadastrar Produto</h1>
-    </PageContainer>
-  );
+    const [image, setImage] = useState<File | null>(null);
+
+    return (
+        <NewProductContainer>
+            <Navbar />
+
+            <main>
+                <h1 className='form-title'>Cadastrar Produto</h1>
+
+                <form className='page-form' action="">
+                    <section className='md:grid md:grid-cols-2 md:gap-6'>
+                        <div className="form-group">
+                            <label
+                                htmlFor="name"
+                                className="block text-sm font-medium text-light-300"
+                            >
+                                Nome
+                            </label>
+                            <input
+                                className="mr-4 w-[18.75rem] text-gray-700 bg-light-300 border-none focus:ring-primary-300 focus:border-primary-300 shadow-sm sm:text-sm rounded-[0.625rem]"
+                                type='text'
+                                id='name'
+                                name='name'
+                                autoComplete="given-product"
+                                placeholder='Nome do produto...'
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label
+                                htmlFor="price"
+                                className="block text-sm font-medium text-light-300"
+                            >
+                                Preço
+                            </label>
+                            <input
+                                className="mr-4 w-[18.75rem] text-gray-700 bg-light-300 border-none focus:ring-primary-300 focus:border-primary-300 shadow-sm sm:text-sm rounded-[0.625rem]"
+                                type='number'
+                                id='price'
+                                name='price'
+                                autoComplete="given-price"
+                                placeholder='Preço'
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label
+                                htmlFor="brand"
+                                className="block text-sm font-medium text-light-300"
+                            >
+                                Marca
+                            </label>
+                            <select
+                                id="brand"
+                                name="brand"
+                                autoComplete="brand-name"
+                                className="py-2 px-3 border w-[18.75rem] text-gray-700 bg-light-300 border-none rounded-[0.625rem] shadow-sm focus:outline-none focus:ring-primary-300 focus:border-primary-300 sm:text-sm"
+                            >
+                                <option>Nike</option>
+                                <option>Adidas</option>
+                                <option>Polo</option>
+                            </select>
+                        </div>
+
+                        <div className="form-group">
+                            <div className="label-text">
+                                Imagem
+                            </div>
+                            <ImageInput 
+                                image={image}
+                                onImageUpdate={(image) => setImage(image)}
+                            />
+                        </div>
+
+                        <button 
+                            type='submit'
+                            className='form-submit'
+                        >
+                            Cadastrar
+                        </button>
+
+                    </section>
+                </form>
+            </main>
+
+        </NewProductContainer>
+    );
 }
---
+++ client/src/pages/NewProduct/style.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const NewProductContainer = styled.div`
+    main {
+        color: var(--light-300);
+        margin: 4rem;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .form-title {
+            font-size: 3rem;
+            font-weight: 600;
+        }
+
+        .page-form {
+            background-color: var(--dark-500);
+            width: 60rem;
+            height: auto;
+            padding: 6.25rem 10rem;
+            border-radius: 1.25rem;
+
+            .label-text {
+                font-size: 0.875rem;
+                font-weight: 500;
+                margin-bottom: 0.5rem;
+            }
+
+            .form-submit {
+                background-color: var(--primary-300);
+                width: 8.5rem;
+                height: 2.4rem;
+                padding: 0.25rem 1rem;
+                border-radius: 0.625rem;
+                color: var(--light-300);
+                font-weight: 600;
+                font-size: 1rem;
+                transition: all 0.2s;
+
+                &:hover {
+                    background-color: var(--primary-500);
+                }
+            }
+        }
+    }
+`;
