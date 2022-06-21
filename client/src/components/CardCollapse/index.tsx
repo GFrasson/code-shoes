24f308b8e416ba94a6cd450c663b3d510d1d8f79
Gabriel Frasson <bielfc27@gmail.com>
2022-06-21 12:48:38 -0300

    Card Collapse component

---
+++ client/src/components/CardCollapse/index.tsx
@@ -0,0 +1,85 @@
+import React, { ReactNode, useState } from "react";
+import { Plus, Minus, Pencil, Trash } from "phosphor-react";
+
+import { CardCollapseContainer } from "./style";
+import { Link } from "react-router-dom";
+
+interface CardCollapseProps {
+    brandId: string;
+    brandName: string;
+    brandImage?: File;
+    products: Product[];
+}
+
+interface Product {
+    id: string;
+    name: string;
+    price: number;
+    image?: File;
+}
+
+export function CardCollapse(props: CardCollapseProps) {
+    const [isCardOpen, setIsCardOpen] = useState(false);
+
+    return (
+        <CardCollapseContainer className={`${isCardOpen && 'open'}`}>
+            <div className="card-visible-content">
+                <img className="brand-image" src="../../../img/brands/nike.png" alt="Logo de uma marca" />
+
+                <div className="title-container">
+                    <h2>{props.brandName}</h2>
+                </div>
+
+                <div className="flex justify-center text-dark-500 actions sm:col-span-1">
+                    <Link to="brands/123-id/edit">
+                        <Pencil size={20} />
+                    </Link>
+                    <Link className="ml-2" to="">
+                        <Trash size={20} />
+                    </Link>
+                    <div className="collapse-button-container">
+                        <button
+                            className="collapse-button"
+                            onClick={() => setIsCardOpen(!isCardOpen)}
+                        >
+                            {
+                                isCardOpen ? <Minus size={16} /> : <Plus size={16} />
+                            }
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-hidden-content">
+                <div className="bg-light-300 shadow overflow-hidden sm:rounded-b-[0.625rem]">
+                    <div className="px-4 py-5 sm:px-6">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">Produtos</h3>
+                    </div>
+                    <div className="border-t border-gray-300">
+                        <dl>
+                            {
+                                props.products.map((product) => {
+                                    return (
+                                        <div key={product.id} className="px-4 py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6 justify-items-center grid-align">
+                                            <img className="h-14 w-14 object-contain sm:col-span-1" src="../../../img/products/nike-blue.png" alt="Imagem de um tênis" />
+                                            <dt className="text-sm font-medium text-dark-300 sm:col-span-1">{product.name}</dt>
+                                            <dd className="mt-1 text-sm text-dark-500 sm:mt-0 sm:col-span-1">R${product.price.toFixed(2)}</dd>
+                                            <div className="flex justify-center text-dark-500 actions sm:col-span-1">
+                                                <Link to={`products/${product.id}/edit`}>
+                                                    <Pencil size={20} />
+                                                </Link>
+                                                <Link className="ml-2" to="">
+                                                    <Trash size={20} />
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </CardCollapseContainer>
+    );
+}
---
+++ client/src/components/CardCollapse/style.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const CardCollapseContainer = styled.div`
+    width: 80%;
+    height: 100%;
+    overflow: hidden;
+
+    -moz-transition: height 0.5s;
+    -ms-transition: height 0.5s;
+    -o-transition: height 0.5s;
+    -webkit-transition: height 0.5s;
+    transition: height 0.5s;
+
+    &.open .card-visible-content {
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .card-visible-content {
+        background-color: var(--light-300);
+        color: var(--dark-500);
+        height: 5rem;
+        border-radius: 0.625rem;
+        padding: 0.5rem 2rem;
+        
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        
+        .brand-image {
+            width: 4rem;
+            height: 4rem;
+            object-fit: contain;
+        }
+        
+        .title-container {
+            width: 4rem;
+        
+            h2 {
+                font-size: 1.5rem;
+                font-weight: 600;
+            }
+        }
+        
+        .collapse-button-container {
+            width: 4rem;
+            display: flex;
+            justify-content: flex-end;
+        }
+    }
+
+    .card-hidden-content {
+        height: 0;
+
+        .grid-align {
+            align-items: center;
+        }
+    }
+
+    &.open .card-hidden-content {
+        height: auto;
+    }
+`;
