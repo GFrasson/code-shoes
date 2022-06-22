d4d1ace98d28ea347e3816bf3e4eceb21c1cec16
Gabriel Frasson <bielfc27@gmail.com>
2022-06-22 11:17:59 -0300

    Dropdown on navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,31 +1,76 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { ShoppingCart } from 'phosphor-react';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { ShoppingCart, SmileySticker, Package, UserGear,  SignIn, SignOut } from 'phosphor-react';
 
 import { NavbarContainer } from './style';
 
 export function Navbar() {
-  return (
-    <NavbarContainer>
-      <nav>
-        <NavLink className='logo' to='/'>
-          <span className='text-secondary-300'>Code</span>
-          <span className='text-primary-300'>Shoes</span>
-        </NavLink>
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
-        <div className='links flex justify-center'>
-          <NavLink to='/'>HOME</NavLink>
-          <NavLink to='/products'>PRODUTOS</NavLink>
-        </div>
+    return (
+        <NavbarContainer>
+            <nav>
+                <NavLink className='logo' to='/'>
+                    <span className='text-secondary-300'>Code</span>
+                    <span className='text-primary-300'>Shoes</span>
+                </NavLink>
 
-        <div className='flex justify-center'>
-          <NavLink className='cart' to='/cart'><ShoppingCart size={30} /></NavLink>
-          <button className='user'>
-            <div className='user-avatar'>
-            </div>
-          </button>
-        </div>
-      </nav>
-    </NavbarContainer>
-  );
+                <div className='links flex justify-center'>
+                    <NavLink to='/'>HOME</NavLink>
+                    <NavLink to='/products'>PRODUTOS</NavLink>
+                </div>
+
+                <div className='flex justify-center'>
+                    <NavLink className='cart' to='/cart'><ShoppingCart size={30} /></NavLink>
+
+                    <nav className="md:flex space-x-10">
+                        <div className="relative">
+                            <button 
+                                className='user'
+                                onClick={() => setDropdownOpen(!dropdownOpen)}
+                            >
+                                <div className='user-avatar'>
+                                </div>
+                            </button>
+
+                            <div className={`${!dropdownOpen && 'hidden'} dropdown absolute z-10 -ml-4 mt-8 transform px-2 w-screen max-w-[15rem] sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2`}>
+                                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                    <div className="relative grid gap-6 bg-dark-500 px-5 py-6 sm:gap-8 sm:p-8">
+                                        <Link to="/admin/products/create" className="-m-3 p-3 flex items-start rounded-lg hover:bg-dark-300">
+                                            <Package size={18} />
+                                            <div className="ml-4">
+                                                <p className="text-base font-medium text-light-300">Cadastrar Produto</p>
+                                                <p className="mt-1 text-sm text-gray-500">Cadastre um novo produto</p>
+                                            </div>
+                                        </Link>
+                                        <Link to="/admin/brands/create" className="-m-3 p-3 flex items-start rounded-lg hover:bg-dark-300">
+                                            <SmileySticker size={18} />
+                                            <div className="ml-4">
+                                                <p className="text-base font-medium text-light-300">Cadastrar Marca</p>
+                                                <p className="mt-1 text-sm text-gray-500">Cadastre uma nova marca</p>
+                                            </div>
+                                        </Link>
+                                        <Link to="/admin" className="-m-3 p-3 flex items-start rounded-lg hover:bg-dark-300">
+                                            <UserGear size={18} />
+                                            <div className="ml-4">
+                                                <p className="text-base font-medium text-light-300">Admin</p>
+                                                <p className="mt-1 text-sm text-gray-500">Acessar a área administrativa</p>
+                                            </div>
+                                        </Link>
+                                        <Link to="/logout" className="-m-3 p-3 flex items-start rounded-lg hover:bg-dark-300">
+                                            <SignOut size={18} />
+                                            <div className="ml-4">
+                                                <p className="text-base font-medium text-light-300">Logout</p>
+                                                <p className="mt-1 text-sm text-gray-500">Sair</p>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </nav>
+        </NavbarContainer>
+    );
 }
